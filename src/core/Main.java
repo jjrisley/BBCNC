e0230856a77df9fde0e298d8510d923a5b9f0fe9
Jarrod Risley <jjrisley@bu.edu>
2017-10-19 01:46:47 -0400

    More print statements.

--- src/core/Main.java
+++ src/core/Main.java
@@ -16,8 +16,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Creating board...");
 		Board board = Platform.createBoard();
 		
+		System.out.println("Getting Pin...");
 		DigitalOutput output = board.getPin(BBBNames.P8_25).as(DigitalOutput.class);
 		
 		System.out.println("Blinking the LED now...");
